a661c2696615327d3a6ce468091e56279a985bb6
gokaydmrl <gokaydemirel96@gmail.com>
2022-07-14 21:21:38 +0300

    master

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,27 +3,37 @@ import React from "react";
 import DeleteSweepRoundedIcon from "@mui/icons-material/DeleteSweepRounded";
 import Checkbox from "@mui/material/Checkbox";
 import { Box } from "@mui/system";
+import "../App.css";
+import Grid from "@mui/material/Grid";
 
 const TodoItem = ({ title, completed, handleChange, removeTodo }) => {
   return (
-    <Box
-      className="item"
-      sx={{
-        justifyContent:"center",
-        margin: "3px",
-        display:"flex"
-        
-      }}
-    >
-      
-          {" "}
-          <Checkbox checked={completed} onChange={handleChange} />
-          {/* <input type="checkbox" checked={completed} onChange={handleChange} /> */}
+    <Box className="item" display="flex">
+      {" "}
+      <Box>
+        <Checkbox checked={completed} onChange={handleChange} />
+      </Box>
+      {/* <input type="checkbox" checked={completed} onChange={handleChange} /> */}
+      <Box>
         <p>{title}</p>
-        <Button endIcon variant="outlined" onClick={removeTodo} size="small">
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "flex-end",
+        }}
+      >
+        <Button
+          sx={{
+            borderRadius: "40%",
+          }}
+          variant="outlined"
+          onClick={removeTodo}
+          size="small"
+        >
           <DeleteSweepRoundedIcon />
         </Button>
-      
+      </Box>
     </Box>
   );
 };
